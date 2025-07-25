d7b461a9cd7b152b384a2464b7dae23534c36d4b
Anton Stiazhkin <rudana089@gmail.com>
2025-07-25 22:13:33 +0300

    Fix comments

--- src/main/java/practice/StreamPractice.java
+++ src/main/java/practice/StreamPractice.java
@@ -44,9 +44,11 @@ public class StreamPractice {
 
     /**
      * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
-     * for example, `Arrays.asList(new Person("Victor", 16, Sex.MAN),
-     * new Person("Helen", 42, Sex.WOMAN))`,
+     * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
+     * new Person(«Helen», 42, Sex.WOMAN))`,
      * select from the List only men whose age is from `fromAge` to `toAge` inclusively.
+     * <p>
+     * Example: select men who can be recruited to army (from 18 to 27 years old inclusively).
      */
     public List<Person> selectMenByAge(List<Person> peopleList, int fromAge, int toAge) {
         return peopleList.stream()
@@ -57,8 +59,13 @@ public class StreamPractice {
 
     /**
      * Given a List of `Person` instances (having `name`, `age` and `sex` fields),
+     * for example, `Arrays.asList( new Person(«Victor», 16, Sex.MAN),
+     * new Person(«Helen», 42, Sex.WOMAN))`,
      * select from the List only people whose age is from `fromAge` and to `maleToAge` (for men)
      * or to `femaleToAge` (for women) inclusively.
+     * <p>
+     * Example: select people of working age
+     * (from 18 y.o. and to 60 y.o. for men and to 55 y.o. for women inclusively).
      */
     public List<Person> getWorkablePeople(int fromAge, int femaleToAge,
                                           int maleToAge, List<Person> peopleList) {
@@ -83,8 +90,16 @@ public class StreamPractice {
     }
 
     /**
-     * Given list of candidates, check which are eligible to apply for president
-     * and return their names sorted alphabetically.
+     * Your help with a election is needed. Given list of candidates, where each element
+     * has Candidate.class type.
+     * Check which candidates are eligible to apply for president position and return their
+     * names sorted alphabetically.
+     * The requirements are: person should be older than 35 years, should be allowed to vote,
+     * have nationality - 'Ukrainian'
+     * and live in Ukraine for 10 years. For the last requirement use field periodsInUkr,
+     * which has following view: "2002-2015"
+     * We want to reuse our validation in future, so let's write our own impl of Predicate
+     * parametrized with Candidate in CandidateValidator.
      */
     public List<String> validateCandidates(List<Candidate> candidates) {
         return candidates.stream()
